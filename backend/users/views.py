839e0ce201d07f8d886415b29534e6a386eb806f
Uday Kiran N <56758575+udaycruise2903@users.noreply.github.com>
2022-10-15 17:35:49 +0530

    init

---
+++ backend/users/views.py
@@ -0,0 +1,18 @@
+#from django.shortcuts import render
+
+# Create your views here.
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import UserSerializer, GroupSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
